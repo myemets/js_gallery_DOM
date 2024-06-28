b6e2a012acf74636b286cd80526164894b4ba447
myemets <for.education1809@gmail.com>
2024-06-28 18:21:57 +0300

    make build

---
+++ index.3b985755.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var n=e.target;if("IMG"===n.tagName&&(n=n.parentElement),"A"===n.tagName){var a=n.getAttribute("href");t.setAttribute("src",a)}})});
+//# sourceMappingURL=index.3b985755.js.map
